342e3a8cc2971ed83a9b305b2811dcda4aab2269
ichang97 <wprakongchai@gmail.com>
2021-01-25 19:34:51 +0700

    Update connect_cta.php

--- admin/connect_cta.php
+++ admin/connect_cta.php
@@ -2,7 +2,7 @@
 <?php
 
 
-$con = mysqli_connect("localhost","dekcomch_cta","Chang5610160**","dekcomch_cta") or die("Error: " . mysqli_error($con));
+$con = mysqli_connect("localhost","xxx","xxxx","xxx") or die("Error: " . mysqli_error($con));
 
 mysqli_query($con,"SET NAMES 'utf8' ");
 
